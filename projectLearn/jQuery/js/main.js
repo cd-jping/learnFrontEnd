e51540a60fd2ca9d2ee999e3b6148af0fde8a5fc
jiping wang <jping1993@outlook.com>
2021-02-10 18:39:22 +0800

    jquery css 完善调整

--- projectLearn/jQuery/js/main.js
+++ projectLearn/jQuery/js/main.js
@@ -1,26 +1,26 @@
-$(function () {
+$(function() {
     'use strict'
     /* 选中页面中所有的input[data-rule] */
-    var $inputs = $('[data-rule]')
-    ,$form =$('#signup')
-    ,inputs =[];
+    var $inputs = $('[data-rule]'),
+        $form = $('#signup'),
+        inputs = [];
 
-    $inputs.each(function (index, node) {
-        inputs.push(new Input (node));
+    $inputs.each(function(index, node) {
+        inputs.push(new Input(node));
     })
 
-    $form.on('submit',function(e){
+    $form.on('submit', function(e) {
         e.preventDefault();
         $inputs.trigger('blur');
 
-        for(var i= 0;i<inputs.length;i++){
+        for (var i = 0; i < inputs.length; i++) {
             var item = inputs[i];
             var r = item.validator.is_valid();
-            if(!r){
+            if (!r) {
                 alert('invalid');
                 return;
             }
         }
+        alert('valid');
     })
 })
-
