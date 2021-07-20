7324b87971f8fc28dbc659e9961c9a63b04a9f4a
jiping Wang <jping1993@outlook.com>
2021-07-20 17:33:54 +0800

    gengxin

--- projectLearn/用ES6写一个计算器/js/main.js
+++ projectLearn/用ES6写一个计算器/js/main.js
@@ -1,1 +1,63 @@
-测试以下 
+const keyboardContainer = document.querySelector('.number-area','.operational-area');
+ 
+const screen = document.querySelector('#screen').value
+ 
+
+let previousNumber = '', currentNumber = '', sign = '';
+
+keyboardContainer.addEventListener('click', e => {
+    const type = e.target.dataset.type
+    console.log(type);
+
+    if (type === 'equal') {
+        calculate()
+    } else if (type === 'operate') {
+        const text = e.target.textContent
+        operate(text)
+    } else if (type === 'clear') {
+        clear()
+    } else {
+        const text = e.target.textContent
+        pushNumber(text)
+    }
+    updateDisplay()
+})
+
+function updateDisplay() {
+    document.getElementById('screen').value = currentNumber;
+}
+
+function pushNumber(num) {
+    currentNumber = currentNumber + num;
+}
+
+function operate(text) {
+    if (currentNumber === '0') return
+    sign = text;
+    previousNumber = currentNumber;
+    // currentNumber = '';
+    switch (sign) {
+      
+        case '+':
+            result = previous + current;
+            break;
+     }
+
+
+}
+function calculate() {
+    let result = 0;
+    const previous = Number(previousNumber);
+    const current = Number(currentNumber);
+    switch (sign) {
+      
+        case '+':
+            result = previous + current;
+            break;
+        default:
+            return;
+
+    }
+    currentNumber = result;
+    sign = ''; 
+}
