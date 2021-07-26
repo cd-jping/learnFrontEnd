73f19a82d98f98128d585cd1d9f2b9ef81ef05db
jiping Wang <jping1993@outlook.com>
2021-07-26 17:17:18 +0800

    重新写了js

--- projectLearn/用ES6写一个计算器-第二版/js/main.js
+++ projectLearn/用ES6写一个计算器-第二版/js/main.js
@@ -43,80 +43,143 @@
 
 
 
+
+//设置键盘绑定监听事件
 const keyboardContainer = document.querySelector('.keyboard');
-const screen = document.querySelector('.display').value
-let displayNumber = '', currentNumber = '', previousNumber = '', sign = '', prevType = '', btnId = '';
+const screen = document.querySelector('#display').value;
+let displayNumber = '', //显示值，接收 计算结果、按钮数字
+    currentNumber = '', //当前值
+    previousNumber = '', //上一个值
+    sign = '';  //操作值（操作符号）
 
 keyboardContainer.addEventListener('click', e => {
+    // 捕获 Id、Class、TextContent的内容 来区分不同的button 执行相应的操作。
+    const textContent = e.target.textContent;
+    const btnId = e.target.id;
+    const className = e.target.className;
 
-    const type = e.target.dataset.type;
-    const text = e.target.textContent;
-    const operateId = e.target.id;
+    //如果Target获取的不是button标签（父级标签）则不处理监听。
     if (e.target.nodeName === 'BUTTON') {
-        if (type === 'operate') {
-            operate(operateId);
-            if (prevType === 'equal') {
-                sign = '';
-                currentNumber = '';
-            }
+        // 事件处理分为监听获取数字button、运算符号、清除等。
+        // 如果是operational类元素 则表示是「操作按钮」；否则就是「数字按钮」。
+        if (className === 'operational') {
+            operational(btnId);
         } else {
-            if (type === 'equal') {
-                calculator();
-                updateDisplay();
-            } else if (type === 'clear') {
-                clear();
-                updateDisplay();
-            } else {
-                if (prevType === 'equal') {
-
-                    currentNumber = '';
-                    previousNumber = '';
-                    pushNumber(text);
-                } else {
-                    pushNumber(text);
-                }
-            }
-
+            pressNumber(textContent);
         }
-
-
     }
-    prevType = type;
-    btnId = operateId;
 })
 
-
+//更新显示
 function updateDisplay() {
-    document.querySelector('.display').value = displayNumber;
+    document.querySelector('#display').value = displayNumber;
 };
 
-function pushNumber(pressNumber) {
-
-    // 小数点输入 和 00000前缀 没有屏蔽
-    document.getElementById('AC').textContent = 'C';
-    currentNumber = currentNumber + pressNumber;
-    displayNumber = currentNumber;
-    updateDisplay();
-}
-function clear() {
+// 清除功能，第一次按 仅清除了当前数 ；第二次按 清除所有数字
+function clean() {
     if (currentNumber != '') {
         displayNumber = '';
         currentNumber = ''
-        document.getElementById('AC').textContent = 'AC';
+        document.getElementById('clean').textContent = 'AC';
     } else {
         currentNumber = '';
-        pressNumber = '';
+        previousNumber = '';
         displayNumber = '';
         sign = '';
     }
+    console.log('Cleared！' + 'prev:' + previousNumber);
 }
 
-function Continuous() {//一个封装方法 是否直接运算？ 连续运算时，判断是否需要计算之前两个值。
-    if (previousNumber != '' && currentNumber != '') { //两个值不为‘’时候才运算。
-        calculator();
+
+//preeNumber 用来接收数字；btn_number 用来获取button标签内的值
+function pressNumber(btn_number) {
+    //有例外情况 为了准确显示 ‘xxx.’情况 临时把值传给placeholder，然后再恢复传给displayNumber
+
+    // 每次输入数字 即将产生当前值，这时候clean键会为「C」；再次按下clean键会变为「AC」
+    document.getElementById('clean').textContent = 'C';
+    //开头不能为「0」
+    if (btn_number === '0' && currentNumber.length == 0 || btn_number === '0' && currentNumber === '-') {  //开头不能为0
+        btn_number = ''; //清空得到的「0」
+    }
+
+    // 如果开头是「.」,处理成「0.」 
+    if (btn_number === '.' && currentNumber.length == 0) {
+        currentNumber = '0.'
+        // 「0.」不给直接传给input；把数字给placeholder
+        document.querySelector('#display').placeholder = String(currentNumber);
+    }
+
+    // 只能输入一个小数点；如果当前数字有「.」则屏蔽后面输入的「.」
+    if (btn_number === '.' && currentNumber.indexOf('.') != -1) {
+        btn_number = '';
+    }
+
+    // 把数字给当前值
+    
+    currentNumber = currentNumber + btn_number;
+
+    // 如果 当前值结尾为「xxx.」
+    if (currentNumber.charAt(currentNumber.length - 1) == '.') {
+        document.querySelector('#display').placeholder = String(currentNumber);
+        displayNumber = ''; //  让placeholder显示出来。
+    } else if(currentNumber=='-') {
+        document.querySelector('#display').placeholder = String(currentNumber+0);
+        displayNumber = ''; //  让placeholder显示出来。
+    }else{
+        displayNumber = currentNumber;
+        document.querySelector('#display').placeholder = '0';
         updateDisplay();
     }
+
+    console.log('curr:' + currentNumber, 'prev:' + previousNumber, 'display:' + displayNumber)
 }
+
+// 操作按钮
+function operational(btn_id) {
+    switch (btn_id) {
+        case 'clean':
+            console.log('pressed:' + btn_id);
+            clean();
+            break;
+        case 'posi-and-nega':
+            if (currentNumber === '') {
+                document.querySelector('#display').placeholder = '-0';
+                currentNumber = '-';
+            } else if (currentNumber === '-') {
+                document.querySelector('#display').placeholder = '0';
+                currentNumber = '';
+            } else {
+                currentNumber = -1 * Number(currentNumber);
+                displayNumber = currentNumber;
+            }
+            console.log('pressed:' + btn_id);
+            break;
+        case 'percent':
+            currentNumber = currentNumber / 100;
+            displayNumber = currentNumber;
+            console.log('pressed:' + btn_id);
+            break;
+        case 'add':
+            console.log('pressed:' + btn_id);
+            break;
+        case 'subtract':
+            console.log('pressed:' + btn_id);
+            break;
+        case 'multiply':
+            console.log('pressed:' + btn_id);
+            break;
+        case 'divide':
+            console.log('pressed:' + btn_id);
+            break;
+        case 'equal':
+            console.log('pressed:' + btn_id);
+            break;
+    }
+    updateDisplay();
+}
+
+
+
 
 function operate(id) {
 
