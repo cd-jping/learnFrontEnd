fa69d7a62ae588b76298551267d7661b5f304cc6
jiping Wang <jping1993@outlook.com>
2021-07-29 17:38:09 +0800

    等待验证 四则运算 把基本运算写出来 再测试

--- projectLearn/用ES6写一个计算器-第二版/js/main.js
+++ projectLearn/用ES6写一个计算器-第二版/js/main.js
@@ -50,8 +50,10 @@ const screen = document.querySelector('#display').value;
 let previousNumber = '', //上一个值
     currentNumber = '', //当前值
     displayNumber = '', //显示值，接收 计算结果、按钮数字
-    preResult = '', //用来存储 遇到二级运算时 保存上一次结果
-    preSign = '', //记录变更运算级别之前的sign
+    bak_preResult = '', // 备份值结果，用来在 各种符号之间切换需要调去原来的计算值
+    bak_sign = '', // 备份符号
+    bak_currentNumber = '', //备份当前数字；
+    bak_previousNumber = '';
     sign = '';  //记录上次运算操作符号
 
 keyboardContainer.addEventListener('click', e => {
@@ -105,15 +107,11 @@ function pressNumber(btn_number) {
         btn_number = ''; //清空得到的「0」
     }
 
-    // || btn_number === '0' && currentNumber === '-'
-
     // 如果开头是「.」,处理成「0.」 
     if (btn_number === '.' && currentNumber.length == 0) {
         currentNumber = '0.'
         // 「0.」不给直接传给input；把数字给placeholder
         document.querySelector('#display').placeholder = String(currentNumber);
-    } else if (btn_number === '.' && currentNumber === '-') {
-        currentNumber = '-0.'
     }
 
     // 只能输入一个小数点；如果当前数字有「.」则屏蔽后面输入的「.」
@@ -147,9 +145,11 @@ function pressNumber(btn_number) {
 
 // 操作按钮
 function operational(btn_id) {
-    // if (currentNumber === '-') {
-    //     currentNumber = '';
-    // }
+    // 把仅为‘-’的currentNumber 重置
+    // 比如按了 posi-and-nega 后没有输入数字，这时候又按了操作按钮 则是视为 ‘’值
+    if (currentNumber === '-') {
+        currentNumber = '';
+    }
     switch (btn_id) {
         case 'clean':
             console.log('pressed:' + btn_id);
@@ -158,7 +158,7 @@ function operational(btn_id) {
         case 'posi-and-nega':
             if (currentNumber === '') {
                 document.querySelector('#display').placeholder = '-0';
-                currentNumber = '-';
+                currentNumber = '-0';
                 console.log(currentNumber);
             } else if (currentNumber === '-') {
                 document.querySelector('#display').placeholder = '0';
@@ -205,57 +205,73 @@ function operational(btn_id) {
 
 // 1获取符号、2判断运算优先级
 function opr_Judgment(curr_sign) { //curr_sign 当前符号
+    // 判断优先级
+    // 可以直接继续计算
+    // 保留上一值 优先运算2级
 
-    if (previousNumber !== '' && currentNumber !== '') {
-        console.log('do calu!')
-        // 判断优先级
-        // 可以直接继续计算
-        // 保留上一值 优先运算2级
+    // 计算 （计算完 要清空 当前值
 
-        // 计算 （计算完 要清空 当前值
-
+    // eg：1+2/ 
+    //   3+    60 * curr='';
+    // 1+2+ 3 * 4*5 *
+    // 从一级运算升到二级运算
+    if ((curr_sign === 'multiply' || curr_sign === 'divide') && (sign === 'add' || sign === 'subtract')) {
+        // previousNumber
+        console.log('1 to 2 !');
+        if(currentNumber!==''){
+            bak_preResult = previousNumber;
+            bak_currentNumber = currentNumber;
+            previousNumber = currentNumber;
+            // currentNumber = '';
+            // bak_sign 用来存储让次变化优先级前的符号
+            bak_sign = sign;
+        }else {
+            previousNumber = bak_previousNumber;
+        }
+    }
+    else
         // 从二级运算降到一级运算
         if ((curr_sign === 'add' || curr_sign === 'subtract') && (sign === 'multiply' || sign === 'divide')) {
-
+            // 1+2+ 3 * 4*5 +
             console.log('2 to 1 !');
+            calculator(sign); //60
+            bak_previousNumber = previousNumber;
+            currentNumber = previousNumber;
+            previousNumber = bak_preResult;
 
-            //currentNumber = previousNumber;
-            // previousNumber = result;
-
-
+            // bak_preResult = '';
+            // previousNumber = bak_preResult;
+            sign = bak_sign;
+            
         }
-        // 从一级运算升到二级运算
-        if ((curr_sign === 'multiply' || curr_sign === 'divide') && (sign === 'add' || sign === 'subtract')) {
-            // previousNumber
-            console.log('1 to 2 !');
+    if (previousNumber !== '' && currentNumber !== '') {
+        console.log('do calu!')
 
-            preResult = previousNumber;
+        calculator(sign);
+        currentNumber = '';
+    } else {
+        if (currentNumber !== '') {
             previousNumber = currentNumber;
             currentNumber = '';
-
-            // preSign 用来存储让次变化优先级前的符号
-            preSign = sign;
+            console.log('Yes!' + previousNumber)
         }
-        calculator(sign);
-
-    } else if (currentNumber !== '') {
-        previousNumber = currentNumber;
-        currentNumber = '';
-        console.log('Yes!' + previousNumber)
-        sign = curr_sign; //把当前符号记录下来 下次计算
+        
     }
-    console.log('preResult: ' + preResult + ' preNumber: ' + previousNumber + ' currNumber: ' + currentNumber + ' display: ' + displayNumber + ' preSign: ' + preSign);
+    sign = curr_sign; //把当前符号记录下来 下次计算
+    console.log('preResult: ' + bak_preResult + ' preNumber: ' + previousNumber + ' currNumber: ' + currentNumber + ' display: ' + displayNumber + ' preSign: ' + bak_sign);
 }
 
 function calculator(sign) {
     switch (sign) {
         case 'add':
-            result = Number(previousNumber) + Number(currentNumber);
-            // previousNumber = result;
+            previousNumber = Number(previousNumber) + Number(currentNumber);
             break;
     }
-    displayNumber = result;
+    
+
 }
+
+
 
 
 // // 1获取符号、2判断运算优先级
